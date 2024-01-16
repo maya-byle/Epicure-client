22503d39283fd67c736c628c01079025463a12e5
mayabyle <mayab@moveo.com>
2024-01-16 17:20:00 +0200

    mobile header draft created

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.scss'; 
+import hamburger from '../../Assets/svg/hamburger.svg'
+import logo from '../../Assets/png/logo.png'
+import magnifying_glass from '../../Assets/svg/magnifying_glass.svg'
+import icon from '../../Assets/svg/icon.svg'
+import shopping_bag from '../../Assets/svg/shopping_bag.svg'
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <img className='mobile_menu_button' src={hamburger} alt='mobile menu'/>
+        <img className='logo' src={logo} alt='logo'/>
+        <div className='right_header'>
+            <img className='icon' src={magnifying_glass} alt='search'/>
+            <img className='icon' src={icon} alt='icon'/>
+            <img className='icon' src={shopping_bag} alt='shopping bag'/>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
