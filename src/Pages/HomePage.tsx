031a8efef95f03fe8461359b9af01c88a1925deb
mayabyle <mayab@moveo.com>
2024-01-21 10:47:12 +0200

    home page styling

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import './homePage.scss';
+
 import Header from "../Components/Header/Header";
 import HeroTitle from "../Components/HeroTitle/HeroTitle";
 import CardsContainer from "../Components/CardsContainer/CardsContainer";
 import Icons from "../Components/Icons/Icons";
+import ChefInfo from "../Components/ChefInfo/ChefInfo";
 
 import restaurantsInfo from '../Constants/resturants';
 import dishesInfo from '../Constants/dishes';
@@ -10,13 +13,14 @@ import iconsInfo from '../Constants/icons';
 
 const HomePage = () => {
     return(
-        <div className="HomePage">
+        <div className="homePage">
             <Header></Header>
-            <div style={{display:'flex', flexDirection:'column', gap:'48px'}}>
+            <div className="feed">
                 <HeroTitle></HeroTitle>
                 <CardsContainer {...restaurantsInfo}/>
                 <CardsContainer {...dishesInfo}/>
                 <Icons icons={iconsInfo}/>
+                <ChefInfo/>
             </div>
         </div>
     )
